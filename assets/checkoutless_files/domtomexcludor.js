2b13f4b495e1637cead8764bc99379a644ce3cf1
Louis Chatriot <lchatriot@stripe.com>
2016-09-13 18:42:46 +0200

    Removed all faulty checkoutless pages

--- assets/checkoutless_files/domtomexcludor.js
+++
@@ -1,8 +0,0 @@
-Validation.add('domtom-excluded','Désolé nous ne livrons pas dans les DOM-TOM.', function(v,elm) {
-    if (document.getElementById(elm.getAttribute('data-country')).value.toLowerCase()=='fr') {
-        return ['971','972','973','974','975','976','984','986','987','988'].indexOf(v.substr(0, 3)) < 0;
-    }
-    else {
-        return true;
-    }
-});
